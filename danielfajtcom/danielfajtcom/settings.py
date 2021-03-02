f63d1ca779e8491eacb03bf1206027f8ac6be6d1
Daniel Fajt <daniel29se@gmail.com>
2021-03-01 23:51:58 -0800

    env

--- danielfajtcom/danielfajtcom/settings.py
+++ danielfajtcom/danielfajtcom/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from ... import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'rq*&11g+9)cn(41sfvk$2@h$8!@@a7j+a4%p7=mknoa_3c6*xb'
+SECRET_KEY = env.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
