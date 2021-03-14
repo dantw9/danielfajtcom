8aa03a91f1b11cf2a237d922d8638d0506d931e3
Daniel Fajt <daniel29se@gmail.com>
2021-03-13 22:56:24 -0800

    models update

--- danielfajtcom/main/models.py
+++ danielfajtcom/main/models.py
@@ -15,16 +15,3 @@ class DailyQuoteModel(models.Model):
 
     def __str__(self):
         return str(self.quote)
-
-
-class DailyBackgroundModel(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, serialize=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    bg_id = models.CharField(max_length=40)
-    bg_author = models.CharField(max_length=40)
-    bg_url = models.CharField(max_length=80)
-    bg_str_b64 = models.TextField(max_length=400000)
-
-    def __str__(self):
-        return str(self.bg_url)
